d8215bc700405af28ebe8b38b54aa2aca02a1a74
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-11-12 00:32:11 +0100

    update

--- src/index.js
+++ src/index.js
@@ -7,11 +7,39 @@ import toast      from './plugins/toast'     ;
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-export default function setupApp(app) {
-
+export function setupApp(app)
+{
     app.use(createPinia());
     app.use(mqttClient);
     app.use(toast);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+export function runApp(app, mountPoint)
+{
+    if(typeof(window.amiWebApp) === 'undefined')
+    {
+        import('bootstrap/dist/css/bootstrap.css').then(() => {
+
+            import('bootstrap-icons/font/bootstrap-icons.css').then(() => {
+
+                import('bootstrap/dist/js/bootstrap.esm.min.js').then((bootstrap) => {
+
+                    window.bootstrap = bootstrap;
+
+                    app.mount(mountPoint);
+                });
+            });
+        });
+    }
+    else
+    {
+        window.ami_mount = (mountPoint) => {
+
+            app.mount(mountPoint);
+        };
+    }
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
