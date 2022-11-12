2e65d212e7796de61716d18825781fc19e55e641
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-11-12 13:00:40 +0100

    update

--- src/plugins/mqttClient.js
+++ src/plugins/mqttClient.js
@@ -161,7 +161,7 @@ class MQTTClient
 
                     }).catch((e) => {
 
-                        reject(e);
+                        this.reconnect();
                     });
                 }
 
