6ce8f3bb72e844437f7df9e52fa60e4a44cc913a
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-11-12 11:20:19 +0100

    update

--- src/components/SignInModal.vue
+++ src/components/SignInModal.vue
@@ -2,17 +2,23 @@
 
 <template>
     <Teleport to="body">
+
         <div class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" id="C2BE0D9F_9779_84D1_EA11_023EBF2CA62F">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
+
                         <h5 class="modal-title">
-                            <i class="bi bi-box-arrow-in-right"></i> Connection
+                            <i class="bi bi-plugin"></i> Connection
                         </h5>
+
                     </div>
                     <div class="modal-body">
+
                         <p>Please fill out the infrastucture information to continue.</p>
+
                         <form id="DA646FB8_5E07_5DE0_1BE4_74A7893F8858">
+
                             <div class="mb-3">
                                 <label class="form-label" for="mqttTokenInput">Token<sup class="text-danger">*</sup></label>
                                 <input class="form-control" type="text" placeholder="Token..." id="mqttTokenInput" autocorrect="off" autocomplete="off" autocapitalize="off" spellcheck="false" required />
@@ -25,16 +31,21 @@
                                 <label class="form-label" for="amiPipelineEndpointInput">AMI-Pipeline URL<sup class="text-muted">opt</sup></label>
                                 <input class="form-control" type="text" placeholder="Endpoint..." id="amiPipelineEndpointInput" autocorrect="off" autocomplete="off" autocapitalize="off" spellcheck="false" optional />
                             </div>
+
                         </form>
+
                     </div>
                     <div class="modal-footer">
+
                         <button class="btn btn-primary" type="submit" form="DA646FB8_5E07_5DE0_1BE4_74A7893F8858">
                             <i class="bi bi-box-arrow-in-right"></i> Connect
                         </button>
+
                     </div>
                 </div>
             </div>
         </div>
+
     </Teleport>
 </template>
 
